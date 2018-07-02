5daa4ec16eea7a5ece62834e8ca36d7a2578f01e
Artur Osypenko <artur.osypenko@dev-pro.net>
2018-07-02 18:11:11 +0300

    add eslint

--- js/main.js
+++ js/main.js
@@ -1,37 +1,13 @@
-const secondComponent = props => {
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'h2',
-      {
-        style: {
-          'font-size': props.fontSize
-        }
-      },
-      'Second component'
-    )
-  );
-};
+const secondComponent = props => React.createElement('div', null,
+  React.createElement('h2', { style: { 'font-size': props.fontSize } }, 'Second component'));
 
-const firstComponent = () => {
-  return React.createElement(
-    'div',
-    null,
-    React.createElement('h1', null, 'My first component!!!'),
-    React.createElement(secondComponent, {
-      fontSize: 18
-    }),
-    React.createElement(secondComponent, {
-      fontSize: 16
-    }),
-    React.createElement(secondComponent, {
-      fontSize: 14
-    })
-  );
-};
+const firstComponent = () => React.createElement('div', null,
+  React.createElement('h1', null, 'My first component!!!'),
+  React.createElement(secondComponent, { fontSize: 18 }),
+  React.createElement(secondComponent, { fontSize: 16 }),
+  React.createElement(secondComponent, { fontSize: 14 }));
 
 ReactDOM.render(
   React.createElement(firstComponent),
-  document.getElementById('app')
+  document.getElementById('app'),
 );
