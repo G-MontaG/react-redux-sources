7c02cc54ff2a60fd59e6246d8bab9e88b7cad6de
Artur Osypenko <artur.osypenko@dev-pro.net>
2018-07-02 19:14:33 +0300

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+      },
+    ],
+  },
 };
