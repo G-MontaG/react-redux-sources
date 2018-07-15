6d72ead9a7bf139ff5b0667a3baf238a4d86ec95
arthur <arthur.osipenko@gmail.com>
2018-07-15 15:40:51 +0300

    browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './pages/landing/Landing';
-
-import './App.css';
 import Coins from './pages/coins/Coins';
 import Converter from './pages/converter/Converter';
 import History from './pages/history/History';
 import News from './pages/news/News';
 import TopExchanges from './pages/topExchanges/TopExchanges';
+import ForOFor from './pages/404';
+import './App.css';
 
 const App = () => (
-  <HashRouter>
+  <BrowserRouter>
     <div className="App">
-      <Route exact path="/" component={Landing} />
-      <Route path="/coins" component={Coins} />
-      <Route path="/converter" component={Converter} />
-      <Route path="/history" component={History} />
-      <Route path="/news" component={News} />
-      <Route path="/top-exchanges" component={TopExchanges} />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/coins" component={Coins} />
+        <Route path="/converter" component={Converter} />
+        <Route path="/history" component={History} />
+        <Route path="/news" component={News} />
+        <Route path="/top-exchanges" component={TopExchanges} />
+        <Route component={ForOFor} />
+      </Switch>
     </div>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 App.displayName = 'App';
