497d0d44e33793e44be7e9f1dcceaabaa95368a3
arthur <arthur.osipenko@gmail.com>
2018-07-18 21:51:32 +0300

    router params

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './Landing.css';
 
-const Landing = () => (
-  <React.Fragment>
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">
-        Welcome to React
-      </h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </React.Fragment>
-);
+const Landing = (props) => {
+  console.log(props);
+  return (
+    <React.Fragment>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">
+          Welcome to React
+        </h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </React.Fragment>
+  );
+};
 
 Landing.displayName = 'Landing';
 
