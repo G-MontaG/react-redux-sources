68ee2646b874413b0fa2162075bdbdfb1cc5d78a
arthur <arthur.osipenko@gmail.com>
2018-07-18 21:40:24 +0300

    nested routing

--- src/pages/topExchanges/TopExchanges.jsx
+++ src/pages/topExchanges/TopExchanges.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Switch, Route, NavLink } from 'react-router-dom';
+import Coins from '../coins/Coins';
+import News from '../news/News';
 
-const TopExchanges = () => (
+const TopExchanges = ({ match }) => (
   <div>
+    <ul>
+      <li>
+        <NavLink to={match.path} activeClassName="active" exact>Main</NavLink>
+      </li>
+      <li>
+        <NavLink to={`${match.path}/coins`} activeClassName="active">Coins</NavLink>
+      </li>
+      <li>
+        <NavLink to={`${match.path}/news`} activeClassName="active">News</NavLink>
+      </li>
+    </ul>
+    <Switch>
+      <Route path={`${match.path}/coins`} component={Coins} />
+      <Route path={`${match.path}/news`} component={News} />
+    </Switch>
     Top Exchanges
   </div>
 );
