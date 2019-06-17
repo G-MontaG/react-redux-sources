8633606979a1250224bcb1918f7cdf433fed4b50
Artur Osypenko <artur.osypenko@dev-pro.net>
2019-06-17 20:08:58 +0300

    Add navigate from coin list to coin page

--- src/components/CoinCard.jsx
+++ src/components/CoinCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import { CRYPTO_COMPARE_URL } from '../constants';
 import withLogProps from '../HOC/withLogProps';
 import './CoinCard.css';
 
-const CoinCard = ({ coin }) => (
+const CoinCard = ({ coin, history }) => (
   <div className="coin-card">
-    <h4>{coin.CoinName}</h4>
+    <h4 onClick={(e) => {history.push(`/coins/${coin.Id}`)}}>{coin.CoinName}</h4>
     {coin.ImageUrl && <img src={`${CRYPTO_COMPARE_URL}${coin.ImageUrl}`} alt={coin.CoinName} />}
   </div>
 );
@@ -15,4 +16,4 @@ CoinCard.propTypes = {
   coin: PropTypes.object.isRequired,
 };
 
-export default withLogProps(CoinCard);
+export default withRouter(withLogProps(CoinCard));
